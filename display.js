88fd88157af92b469b01e665076273c12df4cefb
jorget0rr3s <torresor.jt@gmail.com>
2023-07-08 13:10:48 -0500

    funcinalidad con js

--- display.js
+++ display.js
@@ -3,17 +3,59 @@ class Display {
         this.displayPrevius = displayPrevius; // elemento donde se visualizaran los datos previos
         this.displayCurrent = displayCurrent; // elemento donde se visualizaran los datos actuales
         this.calculador = new Calculadora ();
+        this.tipoOperacion = undefined
         this.previusValue = ''; // valores previus visualizados
         this.currentValue = ''; // valor actual visualizado
+        this.signos = {
+            sumar: '+',
+            restar: '-',
+            dividir: '%',
+            multiplicar: 'x'
+        }
     }
-    
-    numberAdd (numero){
-        this.currentValue = numero;
+    // metodo para borrar el ultimo valor digitado
+    borrar(){
+        this.currentValue = this.currentValue.toString().slice(0,-1);
         this.printValue();
     }
-
+    //metodo para borrar todos los valores del display, y reestablecer la operacion que se estaba realizando
+    allclear(){
+        this.currentValue = '';
+        this.previusValue = '';
+        this.tipoOperacion = undefined;
+        this.printValue();
+    }
+    
+    // metodo para poder agregar un numero al display actual
+    numberAdd (numero){
+        // este condicional permite solo inclur in punto (.) adicional convierte los valores a string  con el metodo 'toString'
+        if (numero === '.' && this.currentValue.includes('.')) return;        
+        this.currentValue = this.currentValue.toString() + numero.toString();
+        this.printValue();
+    }
+    // metodo para imprimir los valores en el display
     printValue (){
         this.displayCurrent.textContent = this.currentValue;
-        this.displayPrevius.textContent = this.previusValue;
+        this.displayPrevius.textContent = `${this.previusValue} ${this.signos[this.tipoOperacion] || ''}`
+    }
+
+    calcular () {
+        // aca se cambian los valores de tipo string a tipo float para poder realizar las operaciones
+        const previusValue = parseFloat(this.previusValue);
+        const currentValue = parseFloat(this.currentValue);
+
+        if (isNaN(previusValue) || isNaN(currentValue)) return;
+        this.currentValue = this.calculador [this.tipoOperacion] (previusValue, currentValue);
+    }
+
+    computar (tipo) {
+        if (this.tipoOperacion !== 'igual') {
+            this.calcular();
+        }
+        this.tipoOperacion = tipo;
+        this.previusValue = this.currentValue || this.previusValue;
+        this.currentValue = '';
+        this.printValue();
+
     }
 }
--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-    const displayPrevius = document.getElementsByClassName('data_previus');
-    const displayCurrent = document.getElementsByClassName('data_current');
+    const displayPrevius = document.getElementsByClassName('data_previus')[0];
+    const displayCurrent = document.getElementsByClassName('data_current')[0];
     const numberButtons = document.querySelectorAll('.number');
     const operationButtons = document.querySelectorAll('.operation');
-    const allClearButton = document.getElementsByClassName('all_clear');
-    const deleteButton = document.getElementsByClassName('delete');
-    const equalButton = document.getElementsByClassName('equals');
+    const allClearButton = document.getElementsByClassName('all_clear')[0];
+    const deleteButton = document.getElementsByClassName('delete')[0];
+    
 
-    const calculadora = new Calculadora();
-    const display = Display (displayPrevius, displayCurrent);
+    
+    const display =new  Display (displayPrevius, displayCurrent);
 
     numberButtons.forEach(boton => {
         boton.addEventListener('click', () => display.numberAdd(boton.innerHTML)   );
     });
+
+    operationButtons.forEach (boton => {
+        boton.addEventListener('click',() => display.computar(boton.value) )
+    })
+
+    deleteButton.addEventListener('click', () => display.borrar());
+    
+    allClearButton.addEventListener('click', () => display.allclear());
 
