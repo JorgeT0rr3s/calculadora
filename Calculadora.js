7489c4d4b1c88ac4ecf07773f6dce3a6833ad214
jorget0rr3s <torresor.jt@gmail.com>
2023-07-08 18:34:51 -0500

    ajustes de funcionamiento

--- Calculadora.js
+++ Calculadora.js
@@ -1,15 +1,24 @@
 class Calculadora {
     sumar(num1, num2){
-        return num1+num2;
+        return parseFloat((num1 + num2).toFixed(5));
     }
     restar(num1, num2){
-        return num1-num2;
+        return parseFloat((num1 - num2).toFixed(5));
     }
     multiplicar(num1, num2){
-        return num1*num2;
+        return (num1 * num2).toExponential(2);
     }
     dividir(num1, num2){
-        return num1 / num2;
+        let a = num1 / num2;
+        let numFormateado
+        if (Math.abs(a) >= 1e6 || Math.abs(a) <= -1e5){
+            numFormateado = a.toExponential(2);
+            return numFormateado
+        }
+        else {
+            return a.toFixed(5);
+        }
+
     }
 
 }
