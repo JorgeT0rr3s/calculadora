658c44b9e7adc93780100e3d71729ae9697a1a9a
jorget0rr3s <torresor.jt@gmail.com>
2023-07-10 15:32:04 -0500

    cambios funcionalidad de la calculadora

--- Calculadora.js
+++ Calculadora.js
@@ -1,24 +1,23 @@
 class Calculadora {
-    sumar(num1, num2){
+    sumar(num1, num2) {
         return parseFloat((num1 + num2).toFixed(5));
     }
-    restar(num1, num2){
+    restar(num1, num2) {
         return parseFloat((num1 - num2).toFixed(5));
     }
-    multiplicar(num1, num2){
-        return (num1 * num2).toExponential(2);
+    multiplicar(num1, num2) {
+        return (num1 * num2);
     }
-    dividir(num1, num2){
+    dividir(num1, num2) {
         let a = num1 / num2;
-        let numFormateado
-        if (Math.abs(a) >= 1e6 || Math.abs(a) <= -1e5){
+        let numFormateado;
+        if (Math.abs(a) >= 1e6) {
             numFormateado = a.toExponential(2);
-            return numFormateado
+        } else {
+            numFormateado = parseFloat(a.toPrecision(3));
         }
-        else {
-            return a.toFixed(5);
-        }
-
+        return numFormateado;
     }
 
 }
+
